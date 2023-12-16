6c395d607eb1a5d2358039b67885d7e94d76f09d
Rohan Dhar <deusexmachina892@gmail.com>
2023-12-17 02:21:57 +0530

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -77,8 +77,8 @@ export default function App() {
   );
 
   // create a state variable for our connection
-  // const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-  const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+  // const connection = new Connection("http://127.0.0.1:8899", "confirmed");
   
   // connection to use with local solana test validator
   // const connection = new Connection("http://127.0.0.1:8899", "confirmed");
